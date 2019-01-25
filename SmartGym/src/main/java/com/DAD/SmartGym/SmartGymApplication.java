fc0292b1e5cf96e767319f46afdd1ca4f9fd1f2e
Sergio Hernandez Dominguez <sergiohd47@gmail.com>
2019-01-25 12:05:12 +0100

    Proyecto nuevo

---
+++ SmartGym/src/main/java/com/DAD/SmartGym/SmartGymApplication.java
@@ -0,0 +1,14 @@
+package com.DAD.SmartGym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartGymApplication.class, args);
+	}
+
+}
+
