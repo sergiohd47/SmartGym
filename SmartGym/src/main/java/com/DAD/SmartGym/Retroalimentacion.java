b1d2eee085bfb16143935bc1e23cf321161e502a
Sergio Hernandez Dominguez <sergiohd47@gmail.com>
2019-01-28 17:05:02 +0100

    Create Retroalimentacion.java

---
+++ SmartGym/src/main/java/com/DAD/SmartGym/Retroalimentacion.java
@@ -0,0 +1,26 @@
+package com.DAD.SmartGym;
+
+public class Retroalimentacion {
+	private boolean valoracion; //TRUE-> ME GUSTA   FALSE-> NO ME GUSTA
+	private String comentario;
+	
+	public Retroalimentacion(boolean valoracion, String comentario) {
+		this.valoracion=valoracion;
+		this.comentario=comentario;
+	}
+	public Retroalimentacion(String comentario) {
+		this.comentario=comentario;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	public boolean getValoracion() {
+		return valoracion;
+	}
+	public void setValoracion(boolean valoracion) {
+		this.valoracion=valoracion;
+	}
+}
