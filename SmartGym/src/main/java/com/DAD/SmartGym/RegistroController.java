e82c1b1ed04ad5c16286d702d3ddd98d734eb671
Sergio Hernandez Dominguez <sergiohd47@gmail.com>
2019-01-29 18:35:22 +0100

    Update RegistroController.java

--- SmartGym/src/main/java/com/DAD/SmartGym/RegistroController.java
+++ SmartGym/src/main/java/com/DAD/SmartGym/RegistroController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RegistroController {
-	@RequestMapping("/#registro")
+	@RequestMapping("/registro")
 	public String registro(Model model) {
 		return "registro.html";
 	}
