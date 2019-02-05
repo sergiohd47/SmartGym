d6598f0b1cdce8147ef88fa1a528bafeffd8fb87
Sergio Hernandez Dominguez <sergiohd47@gmail.com>
2019-02-05 14:26:05 +0100

    Sumary

--- SmartGym/src/main/java/com/DAD/SmartGym/UsuarioBasicoController.java
+++ SmartGym/src/main/java/com/DAD/SmartGym/UsuarioBasicoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UsuarioBasicoController {
-	@RequestMapping("/usuario=usuarioBasico")
+	@RequestMapping("/usuarioBasico")
 	public String usuarioBasico(Model model) {
 		return "usuarioBasico.html";
 	}
--- SmartGym/src/main/java/com/DAD/SmartGym/UsuarioEntrenadorController.java
+++ SmartGym/src/main/java/com/DAD/SmartGym/UsuarioEntrenadorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UsuarioEntrenadorController {
-	@RequestMapping("/acceso?usuario=usuarioEntrenador")
+	@RequestMapping("/usuarioEntrenador")
 	public String usuarioEntrenador(Model model) {
 		return "usuarioEntrenador.html";
 	}
