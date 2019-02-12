5e1a7a7beeb47de501102c8bb00480ae13f849cc
Sergio Hernandez Dominguez <sergiohd47@gmail.com>
2019-02-12 14:06:40 +0100

    Actualizando html

---
+++ SmartGym/src/main/java/com/DAD/SmartGym/EditarPerfilController.java
@@ -0,0 +1,15 @@
+package com.DAD.SmartGym;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EditarPerfilController {
+	@RequestMapping("/editarPerfil")
+	public String acceder(Model model) {
+		return "editarPerfil";
+	}
+	
+}
