47e51aa9191baad4481264638f82635efccd35da
danyelmb <36004911+danyelmb@users.noreply.github.com>
2019-02-20 21:40:31 +0100

    Clase consultas update

--- SmartGym/src/main/java/com/DAD/SmartGym/Repository/ClaseRepository.java
+++ SmartGym/src/main/java/com/DAD/SmartGym/Repository/ClaseRepository.java
@@ -4,8 +4,34 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.DAD.SmartGym.Model.Clase;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ClaseRepository extends JpaRepository<Clase, Long>{
 	List<Clase> findByNombre(String nombre);
+	
+	@Modifying @Transactional
+	@Query("update Clase c set c.intensidadCardio = ?1 where c.nombre = ?2")
+	void setIntensidadCardioByNombre( int intensidadCardio, String nombre);
+	
+	@Modifying @Transactional
+	@Query("update Clase c set c.intensidadFuerza = ?1 where c.nombre = ?2")
+	void setIntensidadFuerzaByNombre(int intensidadFuerza, String nombre);
+	
+	@Modifying @Transactional
+	@Query("update Clase c set c.max_plazas = ?1 where c.nombre = ?2")
+	void setPlazasByNombre(int max_plazas, String nombre);
+	
+	@Modifying @Transactional
+	@Query("update Clase c set c.duracion = ?1 where c.nombre = ?2")
+	void setDuracionByNombre(int duracion, String nombre);
+	
+	@Modifying @Transactional
+	@Query("update Clase c set c.descripcion = ?1 where c.nombre = ?2")
+	void setDescripcionByNombre(String descripcion, String nombre);
+	
+	Clase getByNombre(String nombre);
+	
+	
 }
