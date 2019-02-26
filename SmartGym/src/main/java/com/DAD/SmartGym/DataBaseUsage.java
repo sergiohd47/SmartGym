bd7d82a02962db643b2aa25a3b4c539d29a6a8fd
BorjaPiruXx <bormaral13@gmail.com>
2019-02-26 12:41:02 +0100

    formulario crear nueva clase

--- SmartGym/src/main/java/com/DAD/SmartGym/DataBaseUsage.java
+++ SmartGym/src/main/java/com/DAD/SmartGym/DataBaseUsage.java
@@ -26,7 +26,7 @@ public class DataBaseUsage {
 	public void init(){
 		
 		Usuario user1 = new Usuario("Danny","Molina","danyel@mail.com",'m',"password");
-		/*Usuario user2 = new Usuario("Victory","Saint","markku@mail.com",'f',"rock");
+		Usuario user2 = new Usuario("Victory","Saint","markku@mail.com",'f',"rock");
 		Usuario user3 = new Usuario("Mariete", "Barriga","riddle@mail.com",'m',"wow");
 		Usuario user4 = new Usuario("Juani", "Cano","cernus@mail.com",'f',"cenutria");
 		
@@ -73,7 +73,7 @@ public class DataBaseUsage {
 		trainerRepo.save(trainer1);
 				
 		userRepo.setContrasenaByMail("maggy".toCharArray(), "cernus@mail.com");
-		*/
+		
 	
 		user1.anadirComentario(new Retroalimentacion ("ExtremeFit", true, "Esta muy bien la clase para aquellos que quieran llegar al limite",user1));
 		userRepo.save(user1);
