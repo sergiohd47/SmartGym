b96723492ef018738bfeb5cd88a763dc05fb01aa
danyelmb <36004911+danyelmb@users.noreply.github.com>
2019-03-01 13:30:25 +0100

    metodos repositorio clase

--- SmartGym/src/main/java/com/DAD/SmartGym/Repository/ClaseRepository.java
+++ SmartGym/src/main/java/com/DAD/SmartGym/Repository/ClaseRepository.java
@@ -16,11 +16,35 @@ public interface ClaseRepository extends JpaRepository<Clase, Long>{
 	@Query(value="select nombre from clase", nativeQuery=true)
 	List<String> findAllNombre();
 	
-	/*@Query(value = "select clase.nombre from clase inner join clase_horarios on clase_horarios.clase_id=clase.id inner join horario on horario.id=clase_horarios.horarios_id where horario.dia= ?1 and horario.hora= ?2")
-	Optional<String> findNombreByDiaAndHora(String dia, int hora);*/
+	
+	/*@Query(value = "select clase.nombre from clase, clase_horarios, horario  where clase_horarios.clase_id=clase.id and horario.id=clase_horarios.horarios_id and horario.dia= ?1 and horario.hora= ?2")
+	String findNombreByDiaAndHora(String dia, int hora);*/
+	
+	@Query("select c from Clase c where c.nombre = ?1")
+	Clase getClaseByNombre(String nombre);
 	
 	@Query(value = "select id from clase where clase.nombre = ?1", nativeQuery = true)
 	int getIdByNombre(String nombre);
+	/*
+	@Query(value = "select descripcion from clase where clase.nombre = ?1", nativeQuery = true)
+	String getDescripcionByNombre(String nombre);
+	
+	@Query(value = "select duracion from clase where clase.nombre = ?1", nativeQuery = true)
+	int getDuracionByNombre(String nombre);
+	
+	@Query(value = "select intensidad_cardio from clase where clase.nombre = ?1", nativeQuery = true)
+	int getCardioByNombre(String nombre);
+	
+	@Query(value = "select intensidad_fuerza from clase where clase.nombre = ?1", nativeQuery = true)
+	int getFuerzaByNombre(String nombre);
+	
+	@Query(value = "select max_plazas from clase where clase.nombre = ?1", nativeQuery = true)
+	int getPlazasByNombre(String nombre);
+	
+	@Query(value = "select monitor from clase where clase.nombre = ?1", nativeQuery = true)
+	int getMonitorByNombre(String nombre);*/
+	
+	
 	
 	@Query("select c from Clase c where c.intensidadCardio between :intensidadCardio1 and :intensidadCardio2")
 	List<Clase> findByRangoCardio( int intensidadCardio1, int intensidadCardio2);
